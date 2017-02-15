718e4f3bf266db0c5c400182ef54aa83e299cb0b
maluethi <matthias.luethi@lhep.unibe.ch>
2017-02-15 17:46:15 +0100

    added local environemnt script for uboonecode builds

---
+++ setup_uboonecode_env.sh
@@ -0,0 +1,9 @@
+export LOCAL_PRODUCTS=/home/build/products
+export WORKSPACE=/home/build/workspace
+
+export UBOONE="v06_24_00"
+export QUAL="e10"
+export LARSOFT_QUAL="s43:e10"
+export BUILDTYPE="prof"
+
+

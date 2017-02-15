bbb65a606954ec1ac45b5a3567041119efa67008
maluethi <matthias.luethi@lhep.unibe.ch>
2017-02-15 10:32:43 +0100

    added environemt setup script (for local tests only) for swtrigger builds

---
+++ setup_swtrigger_env.sh
@@ -0,0 +1,13 @@
+export LOCAL_PRODUCTS=/home/build/products
+export WORKSPACE=/home/build/workspace_swtrigger
+
+export BUILD_SCRIPTS=$WORKSPACE/build_scripts
+
+export VERSION="v02_02_03"
+export QUAL="e10"
+export BUILDTYPE="prof"
+
+#mimic git clone on jenkins:
+cp -r  ~/build_scripts $WORKSPACE/
+
+
